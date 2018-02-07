14082fad9afba4da16ecbe0e482e81cf1f83b265
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-02-07 18:02:11 +0530

    Checkout the correct branch before committing
    Prevent creating detached HEAD commits

--- scripts/travis-update-venues.sh
+++ scripts/travis-update-venues.sh
@@ -5,6 +5,7 @@ GIT_URL="https://github.com/punchagan/playo-find-venue.git"
 GIT_URL=$(echo $GIT_URL|sed -e s/github.com/punchagan:"${GITHUB_TOKEN}"@github.com/g)
 
 # Push to GitHub
+git checkout "${TRAVIS_BRANCH}"
 git add data/venues.json
 git commit -m "Travis CI: Auto update venues" --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
 git push --quiet "${GIT_URL}" master:master
