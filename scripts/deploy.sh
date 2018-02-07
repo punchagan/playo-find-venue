eb47d88c477d5725ec69f659dcb1bf22c750c3ec
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-02-07 17:34:53 +0530

    Add travis config and deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+GIT_URL="https://github.com/punchagan/playo-find-venue.git"
+GIT_URL=$(echo $GIT_URL|sed -e s/github.com/punchagan:"${GITHUB_TOKEN}"@github.com/g)
+
+# Push to GitHub
+git add data/venues.json
+git commit -m "Travis CI: Auto update venues" --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
+git push --quiet "${GIT_URL}" master:master
