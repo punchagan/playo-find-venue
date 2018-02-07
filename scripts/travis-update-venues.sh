5ae0719d1c97544dc69cba1c53a5ac3af2ea0eb8
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-02-07 18:06:31 +0530

    Skip ci on auto commits

--- scripts/travis-update-venues.sh
+++ scripts/travis-update-venues.sh
@@ -7,5 +7,6 @@ GIT_URL=$(echo $GIT_URL|sed -e s/github.com/punchagan:"${GITHUB_TOKEN}"@github.c
 # Push to GitHub
 git checkout "${TRAVIS_BRANCH}"
 git add data/venues.json
-git commit -m "Travis CI: Auto update venues" --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
+git commit -m "Travis CI: Auto update venues [ci skip]" \
+    --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
 git push --quiet "${GIT_URL}" master:master
